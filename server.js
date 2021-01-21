2fbc5e48ec96daf5d27f401ed415be48a63d977b
aajayk <aajayk786@gmail.com>
2021-01-21 23:22:34 +0530

    rate limit added

--- server.js
+++ server.js
@@ -4,42 +4,27 @@ const { nanoid } = require("nanoid");
 //const db = require("db");
 require("dotenv").config();
 const MongoClient = require("mongodb").MongoClient;
+const rateLimit = require("express-rate-limit");
+
 const app = express();
 const basePath = process.env.BASE_PATH;
+
+app.set("trust proxy", 1);
+const apiLimiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 15 minutes
+  max: 5,
+  message: "Too many requests, please try again later.",
+});
 
 //mongodb client
 const uri = process.env.URI;
 
 //handle '/favicon.ico' request from chrome
-app.get("/favicon.ico", (req, res) => res.status(204));
-
-//redirect to  LongURL
-app.get("/:id", (req, res) => {
-  console.log(req.url);
-  var baseURL = basePath + req.params.id;
-  console.log("baseurl formed " + baseURL);
-  //mongodb instantiated
-  const client = new MongoClient(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-
-  client.connect(function (err, db) {
-    if (err) throw err;
-    var dbo = db.db("ShortURL");
-    dbo
-      .collection("urlMappings")
-      .findOne({ shortURL: baseURL }, function (err, result) {
-        if (err) throw err;
-        console.log(result);
-        res.redirect(302, result.longURL);
-        db.close();
-      });
-  });
-});
+app.get("/favicon.ico", apiLimiter, (req, res) => res.status(204));
 
 //generating the shortURL
-app.get("/short/it", (req, res) => {
+app.get("/short/it", apiLimiter, (req, res) => {
+  console.log("req received");
   console.log(req.query.longURL);
   if (validUrl.isUri(req.query.longURL)) {
     console.log("Looks like an URI");
@@ -74,6 +59,38 @@ app.get("/short/it", (req, res) => {
     console.log("Not a URI");
     res.status(400).send("malformed longURL");
   }
+});
+
+//redirect to LongURL Route
+app.get("/:id", apiLimiter, (req, res) => {
+  console.log(req.url);
+  var baseURL = basePath + req.params.id;
+  console.log("baseurl formed " + baseURL);
+  //mongodb instantiated
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  client.connect(function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("ShortURL");
+    dbo
+      .collection("urlMappings")
+      .findOne({ shortURL: baseURL }, function (err, result) {
+        if (err)
+          res.status(400).send({
+            error: "something wrong with DB. Please try aafter sometime",
+          });
+        console.log(result);
+        if (result != null) res.redirect(302, result.longURL);
+        else
+          res.status(400).send({
+            error: "shortURL seems incorrect!! please try with valid value",
+          });
+        db.close();
+      });
+  });
 });
 
 app.listen(process.env.PORT || 3003);
